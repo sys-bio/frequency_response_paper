43e95ee4378247b2de73100b8f412930d6cc8d49
joseph-hellerstein <jlheller@uw.edu>
2023-08-26 08:01:06 -0700

    Plot for control analysis

--- plots/make_control_plots.py
+++ plots/make_control_plots.py
@@ -1,10 +1,12 @@
 """Constructs plots for the paper."""
 
 from control.cascade import Cascade
-
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+PLOT_DIR = os.path.dirname(__file__)
 
 def plotSingleControlCoefficientComparisons(species_idx, ax=None):
     size = 4
@@ -14,7 +16,9 @@ def plotSingleControlCoefficientComparisons(species_idx, ax=None):
     r_vals = [0.01, 0.05, 0.1, 0.5, 1, 5, 10, 15, 20, 50, 100, 200, 300, 400, 500]
     cascade.plotSimulatedControlCoefficient(species_idx=species_idx, r_vals=r_vals, ax=ax)
     ax.set_xscale('log')
-    cascade.writePlot("plotControlCoefficientComparisons.pdf")
+    subscript = 2*species_idx-1
+    ax.set_xlabel(r'$log k_{%d}$' % subscript)
+    writePlot("control_coefficient_comparisons.pdf")
 
 def plotMultipleControlCoefficientComparisons():
     nrow = 2
@@ -25,6 +29,16 @@ def plotMultipleControlCoefficientComparisons():
             species_idx = irow * ncol + icol + 1
             ax = axes[irow][icol]
             plotSingleControlCoefficientComparisons(species_idx, ax=ax)
+
+def writePlot(filename):
+    """
+    Writes the current plot to a file.
+
+    Args
+        filename: str 
+    """
+    path = os.path.join(PLOT_DIR, filename)
+    plt.savefig(path)
 
 
 if __name__ == "__main__":
