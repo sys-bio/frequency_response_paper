8956442058395e27c6ebc37f348f05b5669fa940
mialko <kochen.michael@gmail.com>
2023-08-28 09:36:33 -0700

    Plots update.

--- plots/single_cycle_two_layer_plots.py
+++ plots/single_cycle_two_layer_plots.py
@@ -133,9 +133,9 @@ r1 = te.loada("""
 # CCB = []
 #
 # for i in np.arange(1, 2, .001):
+#     r.S = i
 #     r.steadyState()
 #     x.append(r.S)
-#     r.S = i
 #     APs.append(r.AP)
 #     BPs.append(r.BP)
 #     uCCB.append(r.getuCC('BP', 'S') / 10)
@@ -159,14 +159,14 @@ r1 = te.loada("""
 # # axs[0].plot(k1s, uCCA, label="AP CC/10", c="green", linestyle="dashed")
 # # axs[0].plot(k1s, CCA, label="AP scaled CC", c="green", linestyle="dotted")
 #
-# axs[0].plot(x, uCCA, label=r"$\frac{dAP}{dS}\frac{1}{2}$", c="green", linestyle="dashed")
-# axs[0].plot(x, CCA, label=r"$\frac{dAP}{dS}\frac{S}{AP}$", c="green", linestyle="dotted")
+# axs[0].plot(x, uCCA, label=r"$\frac{dAP_{ss}}{dS}\frac{1}{2}$", c="green", linestyle="dashed")
+# axs[0].plot(x, CCA, label=r"$\frac{dAP_{ss}}{dS}\frac{S}{AP_{ss}}$", c="green", linestyle="dotted")
 #
 # # axs[1].plot(k1s, uCCB, label="BP CC/20", c="red", linestyle="dashed")
 # # axs[1].plot(k1s, CCB, label="BP scaled CC/4", c="red", linestyle="dotted")
 #
-# axs[1].plot(x, uCCB, label=r"$\frac{dBP}{dS}\frac{1}{10}$", c="red", linestyle="dashed")
-# axs[1].plot(x, CCB, label=r"$\frac{dBP}{dS}\frac{S}{BP}\frac{1}{2}$", c="red", linestyle="dotted")
+# axs[1].plot(x, uCCB, label=r"$\frac{dBP_{ss}}{dS}\frac{1}{10}$", c="red", linestyle="dashed")
+# axs[1].plot(x, CCB, label=r"$\frac{dBP_{ss}}{dS}\frac{S}{BP_{ss}}\frac{1}{2}$", c="red", linestyle="dotted")
 #
 # axs[2].plot(x, damps, label="damping ratio", c="blue")
 # axs[3].plot(x, BWs, label="bandwidth", c="orange")
@@ -190,75 +190,94 @@ r1 = te.loada("""
 
 # -------------------------------------------------------------
 
-# r.conservedMoietyAnalysis = True
-# r1.steadyState()
+r.conservedMoietyAnalysis = True
+r1.steadyState()
+
+
+x = []
+BWs = []
+damps = []
+APs = []
+BPs = []
+uCCA = []
+CCA = []
+uCCB = []
+CCB = []
+
+for i in np.arange(1, 1.8, .001):
+    r1.S = i
+    r1.steadyState()
+    x.append(r1.S)
+    APs.append(r1.AP)
+    BPs.append(r1.BP)
+    uCCB.append(r1.getuCC('BP', 'S') / 10)
+    CCB.append(r1.getCC('BP', 'S') / 4)
+    uCCA.append(r1.getuCC('AP', 'S') / 3)
+    CCA.append(r1.getCC('AP', 'S'))
+
+    omega_n = np.sqrt((r1.getEE('J1', 'A') + r1.getEE('J2', 'AP'))*(r1.getEE('J3', 'B') + r1.getEE('J4', 'BP')))
+    zeta = 0.5*(r1.getEE('J1', 'A') + r1.getEE('J2', 'AP') + r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))/omega_n
+    omega_c = omega_n*np.sqrt(1-2*zeta**2 + np.sqrt((2*zeta**2-1)**2) + 1)
+    damps.append(zeta)
+    BWs.append(omega_c)
+
+    print(r1.getEE('J1', 'A'), r1.getEE('J2', 'AP'), r1.getEE('J3', 'B'), r1.getEE('J4', 'BP'))
+    # print(i, r1.S, zeta)
+#     a0 = 1/omega_n**2
+#     a1 = (r1.getEE('J1', 'A') + r1.getEE('J2', 'AP') + r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))/(omega_n**2)
+#     a2 = 1
+#     a3 = 0
+#     # if zeta == 1:
+#     #     print()
+#     # print(i, a0, a1, (a1*a2-a0*a3)/a1)
+#     roo = -4/((r1.getEE('J1', 'A') + r1.getEE('J2', 'AP'))*(r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))) \
+#           + ((r1.getEE('J1', 'A') + r1.getEE('J2', 'AP') + r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))**2) \
+#           / (r1.getEE('J1', 'A') + r1.getEE('J2', 'AP'))*(r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))
+#     print(roo)
 #
 #
-# x = []
-# BWs = []
-# damps = []
-# APs = []
-# BPs = []
-# uCCA = []
-# CCA = []
-# uCCB = []
-# CCB = []
-#
-# for i in np.arange(1, 1.8, .001):
-#     r1.steadyState()
-#     x.append(r1.S)
-#     r1.S = i
-#     APs.append(r1.AP)
-#     BPs.append(r1.BP)
-#     uCCB.append(r1.getuCC('BP', 'S') / 10)
-#     CCB.append(r1.getCC('BP', 'S') / 4)
-#     uCCA.append(r1.getuCC('AP', 'S') / 3)
-#     CCA.append(r1.getCC('AP', 'S'))
-#
-#     omega_n = np.sqrt((r1.getEE('J1', 'A') + r1.getEE('J2', 'AP'))*(r1.getEE('J3', 'B') + r1.getEE('J4', 'BP')))
-#     zeta = 0.5*(r1.getEE('J1', 'A') + r1.getEE('J2', 'AP') + r1.getEE('J3', 'B') + r1.getEE('J4', 'BP'))/omega_n
-#     omega_c = omega_n*np.sqrt(1-2*zeta**2 + np.sqrt((2*zeta**2-1)**2) + 1)
-#     damps.append(zeta)
-#     BWs.append(omega_c)
-#
-# fig, axs = plt.subplots(4, figsize=(8, 10))
-# # axs[0].plot(x, APs, label="AP", c="green")
-# # axs[1].plot(x, BPs, label="BP", c="red")
-#
-# axs[0].plot(x, APs, label="$AP_{ss}$", c="green")
-# axs[1].plot(x, BPs, label="$BP_{ss}$", c="red")
-#
-# # axs[0].plot(x, uCCA, label="AP CC/3", c="green", linestyle="dashed")
-# # axs[0].plot(x, CCA, label="AP scaled CC", c="green", linestyle="dotted")
-#
-# axs[0].plot(x, uCCA, label=r"$\frac{dAP}{dS}\frac{1}{3}$", c="green", linestyle="dashed")
-# axs[0].plot(x, CCA, label=r"$\frac{dAP}{dS}\frac{S}{AP}$", c="green", linestyle="dotted")
-#
-# # axs[1].plot(x, uCCB, label="BP CC/10", c="red", linestyle="dashed")
-# # axs[1].plot(x, CCB, label="BP scaled CC/4", c="red", linestyle="dotted")
-#
-# axs[1].plot(x, uCCB, label=r"$\frac{dBP}{dS}\frac{1}{10}$", c="red", linestyle="dashed")
-# axs[1].plot(x, CCB, label=r"$\frac{dBP}{dS}\frac{S}{BP}\frac{1}{4}$", c="red", linestyle="dotted")
-#
-# axs[2].plot(x, damps, label="damping ratio", c="blue")
-# axs[3].plot(x, BWs, label="bandwidth", c="orange")
-#
-# axs[0].tick_params(axis='both', which='major', labelsize=15)
-# axs[1].tick_params(axis='both', which='major', labelsize=15)
-# axs[2].tick_params(axis='both', which='major', labelsize=15)
-# axs[3].tick_params(axis='both', which='major', labelsize=15)
-# plt.xlabel('Signal S', fontsize=15)
-# # plt.title("Damping ratio (single-cycle two-layer system) vs k1")
-# axs[0].legend(fontsize=15)
-# axs[1].legend(fontsize=15)
-# axs[2].legend(fontsize=15)
-# axs[3].legend(fontsize=15)
-# plt.tight_layout()
-# plt.savefig("single_cycle_two_layer_aligned.pdf")
-# # plt.savefig("single_cycle_two_layer_aligned")
-# plt.show()
-#
+# # for i, each in enumerate(x):
+# #     print(x[i], damps[i])
 # quit()
+
+fig, axs = plt.subplots(4, figsize=(8, 10))
+# axs[0].plot(x, APs, label="AP", c="green")
+# axs[1].plot(x, BPs, label="BP", c="red")
+
+axs[0].plot(x, APs, label="$AP_{ss}$", c="green")
+axs[1].plot(x, BPs, label="$BP_{ss}$", c="red")
+
+# axs[0].plot(x, uCCA, label="AP CC/3", c="green", linestyle="dashed")
+# axs[0].plot(x, CCA, label="AP scaled CC", c="green", linestyle="dotted")
+
+axs[0].plot(x, uCCA, label=r"$\frac{dAP_{ss}}{dS}\frac{1}{3}$", c="green", linestyle="dashed")
+axs[0].plot(x, CCA, label=r"$\frac{dAP_{ss}}{dS}\frac{S}{AP_{ss}}$", c="green", linestyle="dotted")
+
+# axs[1].plot(x, uCCB, label="BP CC/10", c="red", linestyle="dashed")
+# axs[1].plot(x, CCB, label="BP scaled CC/4", c="red", linestyle="dotted")
+
+axs[1].plot(x, uCCB, label=r"$\frac{dBP_{ss}}{dS}\frac{1}{10}$", c="red", linestyle="dashed")
+axs[1].plot(x, CCB, label=r"$\frac{dBP_{ss}}{dS}\frac{S}{BP_{ss}}\frac{1}{4}$", c="red", linestyle="dotted")
+
+axs[2].plot(x, damps, label="damping ratio", c="blue")
+axs[3].plot(x, BWs, label="bandwidth", c="orange")
+
+axs[0].tick_params(axis='both', which='major', labelsize=15)
+axs[1].tick_params(axis='both', which='major', labelsize=15)
+axs[2].tick_params(axis='both', which='major', labelsize=15)
+axs[3].tick_params(axis='both', which='major', labelsize=15)
+plt.xlabel('Signal S', fontsize=15)
+# plt.title("Damping ratio (single-cycle two-layer system) vs k1")
+axs[0].legend(fontsize=15)
+axs[1].legend(fontsize=15)
+axs[2].legend(fontsize=15)
+axs[3].legend(fontsize=15)
+plt.tight_layout()
+plt.savefig("single_cycle_two_layer_aligned.pdf")
+# plt.savefig("single_cycle_two_layer_aligned")
+plt.show()
+
+quit()
 
 # =========================================================
 
