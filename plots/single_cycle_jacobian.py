2cd4322f396f5f210ad25570e93a3f36e89a8fb8
mialko <kochen.michael@gmail.com>
2023-09-13 17:31:11 -0700

    Plot update.

---
+++ plots/single_cycle_jacobian.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 10:11:12 2023
+
+@author: hsauro
+"""
+
+import tellurium as te
+import roadrunner
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib.ticker import EngFormatter, StrMethodFormatter
+from freqResponse import *
+import numpy as np
+
+r = te.loada("""
+     v1: A -> AP;  k1*S*A/(Km1 + A)
+     v2: AP -> A;  k2*AP/(Km2 + AP)
+
+     k1 = 0.14
+     k2 = 0.7
+     A = 10
+     Km1 = 0.5
+     Km2 = 0.5
+     S = 1
+     
+""")
+
+r.conservedMoietyAnalysis = True
+r.steadyState()
+
+x = []
+AP = []
+Jac = []
+c1 = []
+c2 = []
+for i in range(800):
+    r.steadyState()
+    x.append(r.S)
+    AP.append(r.AP)
+    Jac.append(r.getReducedJacobian()[0][0])
+    r.S = r.S + 0.01
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('Signal S', fontsize=15)
+ax1.set_ylabel('Jacobian', fontsize=15)
+plt.yticks(fontsize=15)
+plt.xticks(fontsize=15)
+JacPlot = ax1.plot(x, Jac, linewidth=2, label="Jacobian")
+
+# Plot second axis
+ax2 = ax1.twinx()
+ax2.tick_params(labelsize=16)
+APPlot = ax2.plot(x, AP, color='red', linewidth=2, label="AP")
+ax2.set_ylabel('Sigmoid Response', fontsize=15)
+
+lns = JacPlot+APPlot
+labs = [k.get_label() for k in lns]
+ax1.legend(lns, labs, loc="upper left")
+
+fig.tight_layout()
+plt.savefig('Jacobian.pdf')
+plt.savefig('Jacobian')
+plt.show()
